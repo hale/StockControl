c92d4972a8f607cdef01ed94866769aeef435880
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-02-21 17:00:02 +0000

    first commit

---
+++ StockControl.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.HashMap;
+/**
+ * 
+ * 
+ */
+public class StockControl  {
+    private HashMap stock;
+    private HashSet stockNames;
+    
+    public StockControl()  {
+        stock = new HashMap();
+        stockNames = new HashSet();
+    }
+    
+    public addStock(Product product, int quant)  {
+        stock.put( 
+        
+        
+    }
+    
+    private newProduct(String name, int reorderValue, int orderValue)  {
+        
+        
+    }
+
+
+
+
+}
